c0b407a68d1d0eab0919e9df9c712f6cd0759548
ZAWcorp <zaydan2000016020@webmail.uad.ac.id>
2022-04-14 02:27:27 +0700

    articles.json

--- get_json.js
+++ get_json.js
@@ -16,8 +16,19 @@ Vue.createApp({
           console.log(error);
         });
     },
+    getArticles() {
+      axios
+        .get('https://raw.githubusercontent.com/Zaydanaymar/tekweb2022/latihan3/contents/articles.json')
+        .then((res) => {
+          console.log(res.data);
+          this.header = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
   beforeMount() {
-    this.getHeaderData();
+    this.getArticles();
   },
 }).mount('#app');
