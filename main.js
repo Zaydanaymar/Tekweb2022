1954c2f1feeafafb3d0e8faa21c1505d98df21f5
ZAWcorp <zaydan2000016020@webmail.uad.ac.id>
2022-05-17 16:25:55 +0700

    UTS

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
 Vue.createApp({
-    data() {
-      return {
-
-        articles: [],
-      };
+  data() {
+    return {
+      articles: [],
+    };
+  },
+  methods: {
+    getArticleData() {
+      axios
+        .get('https://raw.githubusercontent.com/Zaydanaymar/tekweb2022/main/content/article.json')
+        .then((res) => {
+          console.log(res.data);
+          this.articles = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
-    methods: {
-      getArticleData() {
-        axios
-          .get('https://raw.githubusercontent.com/Zaydanaymar/latihan/master/content/article.json')
-          .then((res) => {
-            console.log(res.data);
-            this.articles = res.data;
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      },
-    },
-    beforeMount() {
-      this.getArticleData();
-    },
-  }).mount('#app');
-  
+  },
+  beforeMount() {
+    this.getArticleData();
+  },
+}).mount('#app');
